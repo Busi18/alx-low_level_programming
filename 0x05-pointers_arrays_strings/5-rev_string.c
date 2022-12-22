e3b55c35c78083ff6c51492a5eb88129e4370da1
Busi18 <Busisiwegadebe18@gmail.com>
2022-12-22 08:02:32 -0800

    Expect the best

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - Prints a string in reverse
+ *
+ * @s: string parameter
+ * Return: no value
+ */
+
+void rev_string(char *s)
+{
+	int rev = strlen(s);
+	int j = 0;
+	char i;
+	while (j < rev / 2)
+	{
+		i = s[j];
+		s[j] = s[rev - j - 1];
+		s[rev - j - 1] = i;
+		j++;
+	}
+}
