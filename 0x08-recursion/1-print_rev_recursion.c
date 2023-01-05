dda1ae6dd90e9a910914baa65752f561f215a752
Busi18 <Busisiwegadebe18@gmail.com>
2023-01-05 14:14:14 -0800

    recursive

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _print_rev_recursion - Prints a string in reverse
  * @s: string
@@ -6,7 +7,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
